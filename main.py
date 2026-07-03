f7138c444fb234cb333d8e8e3e213be46f9af104
Andre <mvuyekureandre@users.noreply.github.com>
2026-07-03 12:23:42 +0200

    add templates and quotes

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+from quotes import quotes
+import random
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello, Andre! This app is deployed.</h1>"
+    quote=random.choice(quotes)
+    return render_template("index.html", quote=quote)
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+# print(random.choice(quotes))
---
+++ quotes.py
@@ -0,0 +1,7 @@
+quotes = [
+    "Talk is cheap. Show me the code.",
+    "Simplicity is the soul of efficiency.",
+    "life is all about winning and losing am a winner",
+]
+for quote in quotes:
+    print(quote)
